0e2693f2cc74f8efa2073f653bf98afde20a21b6
HaydenMcleod9 <hmcle2@ocdsb.ca>
2024-04-18 14:57:32 -0400

    finding stat
    finding stat function is started but not working currently

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 var platform;
+var apisite = '"level": 350,';
+level = "level";
 var number = "ad79003017899a1730d4fd31778d15d4";
 var input;
 var url;
 function othername() {
   input = document.getElementById("userInput").value;
-  url = "https://api.mozambiquehe.re/bridge?auth=" + number + "&player=" + input + "&platform=" + platform;
+  if(input == "kills"){
+  document.getElementById("demo1").innerHTML = "Your a bitch and no one likes you";
+  }else{
+    url = "https://api.mozambiquehe.re/bridge?auth=" + number + "&player=" + input + "&platform=" + platform;
   document.getElementById("demo1").innerHTML = url;
+  }
 }
 function choose(plat) {
   platform = plat;
@@ -15,4 +21,10 @@ function getapi() {
 }
 function test() {
   return url;
+}
+
+function search(stat, place){
+  matches = apisite.match(stat);
+  value = apisite.substring(matches[place]+stat.length()+3, matches[place]+stat.length()+6;
+  document.getElementById("demo3") innerHTML = value;
 }
