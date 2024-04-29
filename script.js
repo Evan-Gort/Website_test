fc9dac055723f9c68142b8bfe9b7696360bb7099
HaydenMcleod9 <hmcle2@ocdsb.ca>
2024-04-29 13:19:27 -0400

    added comments
    .

--- script.js
+++ script.js
@@ -16,25 +16,33 @@ var top3;
 var rankscore;
 var Name;
 
+
+//this function takes in the data entered by the user and stores them in variable and use those varables to make the url
 function othername() {
+  //if statement to check if the user has entered a platform
   if (platform == "none"){
     alert("Please choose a platform");
   }else{
+    //puts the user input into a variable
     input = document.getElementById("userInput").value;
+    //construct the url that contian the account details with the account name and platform selected
     url = "https://api.mozambiquehe.re/bridge?auth=" + number + "&player=" + input + "&platform=" + platform;
     scrapeHTML();
   }
 }
 
-
+//this function activates when one of the radio buttons is clicked and sets a variable according to the platform selected
 function choose(plat) {
   platform = plat;
 }
 
+//this function searches for whatever statistic uses the variable place to determine which mention of the statistic we want to take and then after that we use the variable dist to take the value beside of the statistic mentioned
 function search(stat, place, dist){
   var i = 0;
   var j = 0;
   var match = [];
+  //this loop finds every mention of the statistic and stores it in an array
+  //if no mention it returns -1
   while(i < apisite.length){
     j = apisite.indexOf(stat, i);
     if(j != -1){
@@ -44,8 +52,10 @@ function search(stat, place, dist){
       i = apisite.length;
     }
   }
+  //this constructs the value related to the statistic we want to take
   value = apisite.substring(match[place] + stat.length + dist, match[place] + stat.length + dist + 9);
   i = 0;
+  //stops the string at the mention of a comma or bracket
   while(i < value.length){
     if(value[i] == "," || value[i] == '}' || value[i] == '"'){
       value = value.substring(0,i);
@@ -56,6 +66,7 @@ function search(stat, place, dist){
   return value;
 }
 
+//this function is almost the same as the search function but it takes in the statistic we want to take and then searches after the mention of total to find the total value
 function searchtotal(stat, place, dist){
   var i = apisite.indexOf('"total"');
   var j = 0;
@@ -81,11 +92,7 @@ function searchtotal(stat, place, dist){
   return value;
 }
 
-function findlegend(legend){
-  var place = apisite.indexof(legend);
-  return place;
-}
-
+//all of these funciton finds the values related to the stats and stores them in variables
 function rankScore(){
   return search("Rank", 0, 3);
 }
@@ -129,6 +136,7 @@ function Name(){
   return search("name", 0, 3);
 }
 
+//this function takes the url and uses the fetch function to get the data from the url and then it returns it to the scrapHTML function
 async function fetchHTML(url2) {
   try {
     response = await fetch(url2);
@@ -140,12 +148,13 @@ async function fetchHTML(url2) {
   }
 }
 
+//this function calls on the fetchHTML function and then it takes the data from the fetchHTML function and stores it in a variable
+//after the entirty of the site is stored in a variable we first check to see if the account is valid and then we put all the stats values in variables and after that we display the values
 async function scrapeHTML() {
   url2 = url;
   htmlContent = await fetchHTML(url2);
 
   if (htmlContent) {
-  // Do something with the scraped HTML content
   console.log('Scraped HTML:', htmlContent);
 
   apisite = htmlContent;
@@ -170,6 +179,7 @@ async function scrapeHTML() {
   }
 }
 
+//this function takes the values of the stats and displays them on the screen
 function displaystats(){
   document.getElementById("demo").innerHTML = "Name: " + Name;
   document.getElementById("demo1").innerHTML = "Kills: " + kills;
